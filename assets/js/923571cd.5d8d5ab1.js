9abdfc122ecc1c2581e08e2b65140973165ef80d
wimjongman <wimjongman@users.noreply.github.com>
2021-05-15 10:32:51 +0000

    deploy: ba3cbe0c45c355f1b547974f8f156a02af4f35b9

--- assets/js/923571cd.5d8d5ab1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{106:function(e){e.exports=JSON.parse('{"allTagsPath":"/BIRT-Dev/blog/tags","slug":"compex-commerce","name":"compex commerce","count":1,"permalink":"/BIRT-Dev/blog/tags/compex-commerce"}')}}]);
--- assets/js/d3ca3a2e.b324f36f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{117:function(a){a.exports=JSON.parse('{"allTagsPath":"/BIRT-Dev/blog/tags","slug":"o-sbea","name":"osbea","count":1,"permalink":"/BIRT-Dev/blog/tags/o-sbea"}')}}]);
